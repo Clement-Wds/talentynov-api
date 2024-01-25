6e49a6bd7a72da0db91b4a696fead1914271faa8
Clément Wds <71884576+Clement-Wds@users.noreply.github.com>
2024-01-25 15:12:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+    console.error("La clé secrète JWT n'est pas définie dans le fichier .env");
+    process.exit(1);
+}
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
